8d948c5d40b1edac230252d9cfc25204ab7e5389
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-10 15:05:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,65 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  pigeon_list = {
-    "Theo"=> {
-      :color =>
-      ["purple", "grey"],
-      :gender =>
-      ["male"],
-      :lives =>
-      ["Subway"],
-    },
-    "Peter Jr." => {
-      :color =>
-      ["purple", "grey"],
-      :gender =>
-      ["male"],
-      :lives =>
-      ["Library"],
-    },
-    "Lucky" => {
-      :color =>
-      ["purple"],
-      :gender =>
-      ["male"],
-      :lives =>
-      ["Central Park"],
-    },
-    "Ms. K" => {
-    :color =>
-    ["grey", "white"],
-    :gender =>
-    ["female"],
-    :lives =>
-    ["Central Park"],
-  },
-  "Queenie" => {
-    :color =>
-    ["white", "brown"],
-    :gender =>
-    ["female"],
-    :lives =>
-    ["Subway"],
-    },
-    "Andrew" => {
-      :color =>
-      ["white"],
-      :gender =>
-      ["male"],
-      :lives =>
-      ["City Hall"],
-    },
-    "Alex" => {
-      :color =>
-      ["white", "brown"],
-      :gender =>
-      ["male"],
-      :lives =>
-      ["Central Park"],
 
-    },
-  }
-  
+binding.pry  
 end
 
 
