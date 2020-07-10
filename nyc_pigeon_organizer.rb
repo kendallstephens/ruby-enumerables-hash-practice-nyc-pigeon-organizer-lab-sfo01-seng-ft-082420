8b1c18152a9eae5757ab940c76cb2f3fc06df136
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-10 15:28:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 data.each_with_object({}) do |(key, value), final_array|
  value.each do |inner_key, names|
-   names.each do |names|
+   names.each do |name|
+     if !final_array
 end        
 
 binding.pry  
